482e808acffb1920417f7540e8f1a608bbf6c249
Canthar90 <d.cieslak90@gmail.com>
2022-12-21 16:06:18 +0100

    Added columns with projects nothing much to display yet but soon will be more

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 
 st.set_page_config(layout="wide")
@@ -16,7 +17,28 @@ with col2:
     Now I'm workinga as Software localization engeener. Programming is my passion in any language as Automation engeener i was working with Assembly lvl languages.
     I was able to completly rewrite one machine, and update several more. 
     In my current job im Preparing XLM files for translation and checking them aftre translation.
-    Currently I'm working with Python in every free momment, It's great fun. Im hoping to find job with Python.
+    Currently I'm working with Python in every free momment, It's great fun. Im hoping to find job with It.
      
     """
     st.info(content)
+    
+st.write("Below you can find some apps written by me. If you like it feel fre to contact me!")
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[0::2].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image(row["image"], width=250)
+        st.write(f"[Go to app]({row['url']})")
+        
+        
+with col4:
+    for index, row in df[1::2].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image(row["image"], width=250)
+        st.write(f"[Go to app]({row['url']})")
