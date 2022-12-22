68720938603d8c9d3b6e2b6f24d046005afe5f34
Canthar90 <d.cieslak90@gmail.com>
2022-12-22 13:19:38 +0100

    Added email sending function

--- pages/Contact_us.py
+++ pages/Contact_us.py
@@ -2,3 +2,11 @@ import streamlit as st
 
 
 st.header("Contact Me")
+
+with st.form("contact_form"):
+    user_email = st.text_input("Your email address")
+    user_message = st.text_area("Your message here")
+    button = st.form_submit_button("Submit")
+    if button:
+        user_message = user_message + '\n' + user_email
+        print("I was pressed")
---
+++ send_email.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+import smtplib, ssl
+
+load_dotenv("env.env")
+
+USERNAME = os.getenv('EMAIL_USERNAME')
+PASS = os.getenv("EMAIL_PASS")
+HOST = "smtp.gmail.com"
+PORT = 465
+
+reciver = USERNAME
+
+message = """
+Subject: Contact form message!
+Whats up m8
+Bye
+"""
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL(HOST, PORT, context=context) as server:
+    server.login(USERNAME, PASS)
+    server.sendmail(USERNAME, reciver, message)
+
+
