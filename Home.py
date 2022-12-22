1aed10bf682e402a922da549d4e72420ca0e4209
Canthar90 <d.cieslak90@gmail.com>
2022-12-22 16:37:15 +0100

    Problems with photo

--- Home.py
+++ Home.py
@@ -8,7 +8,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("miages\photo.png", width=300)
+    st.image("miages/photo.png", width=300)
     
 with col2:
     st.title("Dawid Cieślak")
